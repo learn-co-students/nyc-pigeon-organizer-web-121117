b49b704f2bf0753f4fc27ece7f87fbc0139c0cfa
Kenny Hom <kenny.hom27@gmail.com>
2017-11-21 23:49:44 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,9 +1,9 @@
 def nyc_pigeon_organizer(data)
   pigeon_list = {}
-  data.map {|trait, set| set.map { |attr,names| names.map { |name| pigeon_list[name] = {} } } }
   data.map do |trait, set|
     set.map do|attr, names|
     names.each do |name|
+      pigeon_list[name] ||= {}
       pigeon_list[name][trait] ||= []
       pigeon_list[name][trait] << attr.to_s
       end
