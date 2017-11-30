39c8b13f29850da5d691d527c045bf609490c5f9
Joshua Ticzon <ticzon.joshua@gmail.com>
2017-11-30 02:21:26 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,30 +2,18 @@ require 'pry'
 
 def nyc_pigeon_organizer(data)
   hash = Hash.new
-  data.each do |stats|
-    stats.values.each |specificStat|
-      specificStat.values.each do |name|
-    binding.pry
-    end
-  end
-end
-
-
-def reformat_languages(languages_by_style)
-  hash = Hash.new
-  languages_by_style.each do |styles|
-    styles.each_with_index do |style, i|
-      styles[1].each do |language, types|
-        hash[language] = {}
-          types.each do |type, value|
-          hash[language] = {type => value}
-          hash[language][:style] = [styles[0]]
-          if language == :javascript
-            hash[language][:style] = languages_by_style.keys
-          end
+  data.each_with_index do |stats, i|
+    stats[1].each_with_index do |specificStat, j|
+      specificStat[1].each do |name|
+          if !hash.keys.include?(name)
+            hash[name] = {data.keys[i] => [stats[1].keys[j].to_s]}
+          elsif !hash[name].keys.include?(stats[0])
+            hash[name][data.keys[i]] = [stats[1].keys[j].to_s]
+          elsif hash.keys.include?(name)
+            hash[name][stats[i]] << stats[1].keys[j].to_s
         end
       end
     end
   end
-  return hash
+return hash
 end
