a7676b3f65974e4d907a2a199def54ec16ef4bd7
Maurice Roy <mautayro@gmail.com>
2017-12-03 03:45:13 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,26 @@
+require "pry"
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_list = {}
+  data.each do |category, category_data|
+    category_data.each do |attribute, pigeons| #creates containers
+      pigeons.each do |pigeon|
+        if pigeon_list.keys.include?(pigeon)
+          #bad code here?
+        else
+          pigeon_list[pigeon] = {}
+        end
+      end
+    end
+    pigeon_list.each do |pigeon, pigeon_data| #inserts data
+      pigeon_data.merge!(category => [])   #add color,gender,lives
+      category_data.each do |attribute, pigeons|
+        if pigeons.include?(pigeon)
+          pigeon_data[category] << attribute.to_s
+        end
+      end
+    end
+    #binding.pry
+  end
+  pigeon_list
 end
